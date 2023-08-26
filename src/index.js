efafda68db17d10fe958dc7d7481e11ce49f1022
nhattgy <nhattsvd@gmail.com>
2023-08-26 17:16:06 +0700

    change update react

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client"; // Thay đổi dòng này
+
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import store from "./store";
 import { Provider } from "react-redux";
-ReactDOM.render(
+
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
